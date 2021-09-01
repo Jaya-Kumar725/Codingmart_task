ac31f12cf957ba98ac0d0909ad861fb7d7fe1114
Jaya kumar N <67946925+Jaya-Kumar725@users.noreply.github.com>
2021-09-01 20:57:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+import React from "react"
+import Company from "./Company";
+import Branch from "./Branch";
+
+
+
+export default function App() {
+  return (
+    <div className="App">
+     <Company/>
+     <Branch/>
+      </div>
+  );
+}
